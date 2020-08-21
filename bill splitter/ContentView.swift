45bd591b36b4ac5b3c326e62f4d9e62836ed9a7c
Swamita <swamita2001@gmail.com>
2020-08-22 01:16:00 +0530

    Initial Commit

---
+++ bill splitter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  bill splitter
+//
+//  Created by Swamita on 22/08/20.
+//  Copyright © 2020 Swamita Gupta. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
